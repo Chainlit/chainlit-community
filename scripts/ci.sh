b6dde2702083ca9e55ff5ae0234b167d6533aed6
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2025-01-28 11:01:35 +0000

    CI convenience shell script.

---
+++ scripts/ci.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+### ci convenience shell script.
+### Usage: ./ci.sh [command]
+### If no command is provided, the script will run the following commands:
+### ruff format --check
+### ruff check
+### pytest
+### pyright
+### Otherwise, the script will run `uv run --all-packages $command`.
+
+UV="uv run --all-packages"
+
+if [ -n "$1" ]; then
+  $UV $1
+  exit 0
+fi
+
+set -ex
+$UV ruff format --check
+$UV ruff check
+$UV pytest
+$UV pyright
